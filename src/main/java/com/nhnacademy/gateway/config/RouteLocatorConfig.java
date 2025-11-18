6a8a5adff413b1fc2c12a8b9789522fc3eb9eb39
serrot <parksh568923@gmail.com>
2025-11-18 22:48:08 +0900

    Locator 간략 정의, eureka 정상화로 yml 적용

---
+++ src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
@@ -0,0 +1,36 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + * While every precaution has been taken in the preparation of this resource,  assumes no
+ * + responsibility for errors or omissions, or for damages resulting from the use of the information
+ * + contained herein
+ * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
+ * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
+ * + prior written permission.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+
+package com.nhnacademy.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfig {
+
+    @Bean
+    public RouteLocator myRoute(RouteLocatorBuilder builder) {
+
+        RouteLocator routeLocator = builder.routes().build();
+
+
+        return builder.routes()
+                .route("order-service",
+                        p -> p.path("/api/order").and()
+                                .uri("lb://order-service")
+                )
+                .build();
+    }
+}
