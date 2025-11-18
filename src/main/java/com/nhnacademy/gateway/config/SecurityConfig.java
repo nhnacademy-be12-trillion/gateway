7b2ab1abab6b413c5b9dedbf1e940ce8ee64670f
serrot <parksh568923@gmail.com>
2025-11-19 01:52:43 +0900

    개발용 permit all

--- src/main/java/com/nhnacademy/gateway/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/gateway/config/SecurityConfig.java
@@ -14,24 +14,24 @@ package com.nhnacademy.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange(exchanges -> exchanges
-                        .anyExchange().permitAll() // 모든 요청 허용
-                )
-                .csrf(ServerHttpSecurity.CsrfSpec::disable) // CSRF 비활성화
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable) // HTTP Basic 비활성화
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable) // 폼 로그인 비활성화
-                .build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().permitAll()
+        );
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
 }
