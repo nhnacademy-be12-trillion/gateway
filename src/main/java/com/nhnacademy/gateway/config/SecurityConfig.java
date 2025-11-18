0dd0e910e9c904d37e66be84a4a63f3a64277382
serrot <parksh568923@gmail.com>
2025-11-18 13:43:53 +0900

    spring cloud 의존성 호환 해결

--- src/main/java/com/nhnacademy/gateway/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/gateway/config/SecurityConfig.java
@@ -14,26 +14,24 @@ package com.nhnacademy.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(authorizeRequests ->
-                authorizeRequests.anyRequest().authenticated()
-        );
-
-        http.httpBasic(Customizer.withDefaults());
-
-        return http.build();
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll() // 모든 요청 허용
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // CSRF 비활성화
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable) // HTTP Basic 비활성화
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable) // 폼 로그인 비활성화
+                .build();
     }
 
 }
