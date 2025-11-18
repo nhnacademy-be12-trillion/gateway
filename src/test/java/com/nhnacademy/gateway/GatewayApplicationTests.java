4b68e4cb82493c890fb1b0e0f9cf83f2d140c8c5
serrot <parksh568923@gmail.com>
2025-11-18 12:49:13 +0900

    config server 설정 제거, eureka 설정

--- src/test/java/com/nhnacademy/gateway/GatewayApplicationTests.java
+++ src/test/java/com/nhnacademy/gateway/GatewayApplicationTests.java
@@ -14,10 +14,8 @@ package com.nhnacademy.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class GatewayApplicationTests {
 
 	@Test
