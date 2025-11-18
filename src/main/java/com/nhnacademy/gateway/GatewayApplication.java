53e84ebf953d0269f7a01fc1f78d03faf8923c91
serrot <parksh568923@gmail.com>
2025-11-18 12:49:13 +0900

    config server 설정 제거, eureka 설정

--- src/main/java/com/nhnacademy/gateway/GatewayApplication.java
+++ src/main/java/com/nhnacademy/gateway/GatewayApplication.java
@@ -14,11 +14,10 @@ package com.nhnacademy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@ConfigurationPropertiesScan
+
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
--- src/main/java/com/nhnacademy/gateway/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/gateway/config/SecurityConfig.java
@@ -14,6 +14,7 @@ package com.nhnacademy.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -26,7 +27,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated()
+        );
 
+        http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
