19b264db1a5a524c4f79f7e06686d8438d1b8328
serrot <parksh568923@gmail.com>
2025-12-16 01:53:25 +0900

    SSL 가상 인증서 구현
    feat. 로컬 환경 쿠키 secure 세팅 대응(비회원, JWT 토큰 등)

--- src/main/java/com/nhnacademy/gateway/GatewayApplication.java
+++ src/main/java/com/nhnacademy/gateway/GatewayApplication.java
@@ -12,9 +12,16 @@
 
 package com.nhnacademy.gateway;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 
 @EnableDiscoveryClient
@@ -23,6 +30,33 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		return tomcat;
+	}
+
+	private Connector httpToHttpsRedirectConnector(){
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(10400);
+		return connector;
 	}
 
 }
