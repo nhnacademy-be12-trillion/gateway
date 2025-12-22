92bfd73372c0f9519131d835b0266c559c3e1eb0
chosun-nhn18 <kimshh21@gmail.com>
2025-12-22 15:32:55 +0900

    chore: auth-service에 소셜 로그인 경로 추가, member-service에 비밀번호 재설정 경로 추가

--- src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/gateway/config/RouteLocatorConfig.java
@@ -59,7 +59,7 @@ public class RouteLocatorConfig {
 
         return builder.routes()
                 .route("auth-service",
-                        p -> p.path("/api/auth/**")
+                        p -> p.path("/api/auth/**", "/api/login/**")
                                 .filters(f -> f
                                         .stripPrefix(1)
                                         .filter(authorizationFilter.apply(guestAllowedConfig)))
@@ -69,7 +69,8 @@ public class RouteLocatorConfig {
                                         "/api/members/signup",
                                         "/api/members/dormant/**",
                                         "/api/members/emails/**",
-                                        "/api/members/findEmail"
+                                        "/api/members/findEmail",
+                                        "/api/members/password/**"
                                 )
                                 .filters(f -> f
                                         .stripPrefix(1)
